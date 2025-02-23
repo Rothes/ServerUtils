1d8a22b8f16136320ff42a072cfa04d8884ab66f
Rothes <449181985@qq.com>
2025-02-23 19:22:37 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ subprojects {
         mavenCentral()
         maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots")
-        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://repo.papermc.io/repository/maven-public/")
         maven("https://libraries.minecraft.net")
     }
 
